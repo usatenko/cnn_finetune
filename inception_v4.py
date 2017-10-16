e22ba676e05f78a6628887f280af1c3a18e745ed
au <alexeyu@aspentechlabs.com>
2017-10-16 09:16:03 +0300

    upgraded to keras 2

--- inception_v4.py
+++ inception_v4.py
@@ -20,6 +20,10 @@ def get_session(gpu_fraction=0.98):
     return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 ktf.set_session(get_session())
 
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config = config)
+
 # ### start session
 # config=tf.ConfigProto()
 # # config.gpu_options.per_process_gpu_memory_fraction=0.98
